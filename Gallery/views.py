83a4efc34a5d18da97a228dcf390a2d54a7f3ad6
munganyendesandrine <mungasandwingsd@gmail.com>
2019-03-15 13:53:37 +0200

    views updated

--- Gallery/views.py
+++ Gallery/views.py
@@ -3,10 +3,6 @@ from django.http  import HttpResponse
 from .models import Image
 
 
-# Create your views here.
-# def welcome(request):
-#     return render(request, 'welcome.html')
-
 def gallery_today(request):
     gallery = Image.objects.all()
     return render(request, 'all-galleries/today-gallery.html', {"gallery": gallery}) 
